5bcc80ffa045aef9e13e554dc853777d16ae7c63
SouL-H <mhmtaydinw@gmail.com>
2021-10-19 23:58:03 +0300

    Square func

--- type-alias.go
+++ type-alias.go
@@ -1,15 +1,23 @@
 package main
 
 type Yazi string
-
+type Sonuc int
 func (k Yazi) Yaz() {
 	println(k)
+}
+func (y Sonuc) Hesapla(){
+	print("Girilen değerin karekökü:",(y*y))
+
 }
 func main() {
 	var gelen Yazi
 	gelen = "Lorem"
 	//Bu şekilde stringe yeni yetenekler kazandırabildik.
 	gelen.Yaz()
-	// byte uint8'in alias'ıdır. 
+	var snc Sonuc
+	snc=6
+	snc.Hesapla()
+
+	// byte uint8'in alias'ıdır.
 
 }
